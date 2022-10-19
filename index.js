19b33056d33dbdf9bc48504af2d13dabd07d0623
oyebodemax <bodokunfolami@gmail.com>
2022-10-19 15:25:00 +0100

    Improved index.js

--- index.js
+++ index.js
@@ -2,59 +2,58 @@ const questionElem = document.querySelector("#question");
 const leftListElem = document.querySelector("#left-list");
 const rightListElem = document.querySelector("#right-list");
 const optionElemList = document.querySelectorAll(".option");
+const successAlert = document.querySelector("#success");
+const dangerAlert = document.querySelector("#danger");
+const submitBtn = document.querySelector("#submit");
+const nextBtn = document.querySelector("#next");
 // Data
 const questions = [
   {
     question: "Which of these programing language is not dynamically typed?",
-    a: "Java",
-    b: "Ruby",
-    c: "Python",
-    d: "Javascript",
+    options: ["Java", "Ruby", "Python", "Javascript"],
     answer: "Java",
   },
   {
     question: "Python was created by Guido Van Rossum in what year?",
-    a: "2005",
-    b: "1984",
-    c: "1991",
-    d: "1992",
+    options: ["2005", "1984", "1991", "1992"],
     answer: "1991",
   },
   {
     question:
       "The runtime environment that enables javascript to run on the server is called?",
-    a: "Aplinejs",
-    b: "webpack",
-    c: "Babel",
-    d: "Nodejs",
+    options: ["Aplinejs", "webpack", "Babel", "Nodejs"],
     answer: "Nodejs",
   },
 ];
 
-function getRadioElem(value) {
-  return `<input type="radio" name="option" id="${value}" value="${value}">`;
-}
-
-function createDivElem(radio, option) {
-  return `<div>${radio} ${option}</div>`;
-}
-
 let currentIndex = 0;
-let ques = questions[currentIndex];
 
-questionElem.insertBefore(
-  document.createTextNode(questions[currentIndex].question),
-  null
-);
+function showCurrentQuestion() {
+  let ques = questions[currentIndex];
+  questionElem.innerHTML = ques.question;
 
-let radio = getRadioElem;
+  optionElemList.forEach((elem, index) => {
+    elem.children[0].value = ques.options[index];
+    elem.children[1].innerHTML = ques.options[index];
+  });
+}
 
-optionElemList.forEach((elem) => {
-  elem.insertBefore(createTextNode(ques));
+submitBtn.addEventListener("click", (e) => {
+  let ques = questions[currentIndex];
+  const radioElem = document.querySelector("input[type=radio]:checked");
+  if (radioElem.value == ques.answer) {
+    successAlert.classList.remove("d-none");
+    dangerAlert.classList.add("d-none");
+  } else {
+    dangerAlert.classList.remove("d-none");
+    successAlert.classList.add("d-none");
+  }
 });
 
-// // prettier-ignore
-// leftListElem.innerHTML = `${createDivElem(radio(ques.a), ques.a)}${createDivElem(radio(ques.b), ques.b)}`;
+nextBtn.addEventListener("click", () => {
+  if (currentIndex === questions.length - 1) return;
+  currentIndex++;
+  showCurrentQuestion();
+});
 
-// // prettier-ignore
-// rightListElem.innerHTML = `${createDivElem(radio(ques.c), ques.c)}${createDivElem(radio(ques.d), ques.d)}`;
+showCurrentQuestion();
